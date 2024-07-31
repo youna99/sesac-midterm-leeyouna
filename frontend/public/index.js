242d7212939597b9f24584d7dab60d89b2e29232
youna99 <dbsk0108@gmail.com>
2024-07-31 16:10:13 +0900

    5

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,11 +1,15 @@
-function getTodos() {
-    axios({
-        method: get,
-        url: "https://jsonplaceholder.typicode.com/todos"
-    })
-    .then((res) => {
-    console.log(res);
-})
+async function getTodos() {
+    try {
+        const res = await axios ({
+            method: 'get',
+            url: "https://jsonplaceholder.typicode.com/todos"
+        })
+        console.log(res.data);
+        let list = res.data.slice(0, 10);
+        const newLi = document.createElement('li');
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
@@ -56,16 +60,16 @@ let result = document.getElementById('result');
 //     })
 // }
 
-const addBtn = document.querySelector('#addBtn'); // 추가 버튼 선택
+const addBtn = document.querySelector('#addBtn');
 
-function keyCodeCheck (event) { // 엔터키로 추가
+function keyCodeCheck (event) {
 	if(document.event.keyCode === 13 && todoInput.value !== ''){
         addTodo();
     }
 }
 
 addBtn.addEventListener('click', () => {
-    if(todoInput.value !== ''){ // 빈 값 입력 방지
+    if(todoInput.value !== ''){
         addTodo();
     }
 })
@@ -77,17 +81,18 @@ function addTodo () { // 할 일 추가 기능
 	const newSpan = document.createElement('span'); // span 생성
 	const todoInput = document.querySelector('#todoInput');
     let del = document.createElement('button');
+    newLi.appendChild(del);
     del.innerText = "x";
     del.addEventListener("click", deleteList);
       
     newLi.appendChild(newBtn); // li안에 button 담기
 	newLi.appendChild(newSpan); // li안에 span 담기
       
-	newSpan.textContent = todoInput.value; // span 안에 value값 담기
+	newSpan.textContent = todoInput.value;
       
 	todoList.appendChild(newLi);
       
-	todoInput.value = ''; // value 값에 빈 문자열 담기
+	todoInput.value = '';
   
 	newBtn.addEventListener('click', () => { // 체크박스 클릭시 완료 표시
 		newLi.classList.toggle('complete');
