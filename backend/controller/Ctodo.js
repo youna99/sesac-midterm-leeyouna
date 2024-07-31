a04caa18f7efd9668afe73767b22599a03d1e88d
youna99 <dbsk0108@gmail.com>
2024-07-31 18:36:11 +0900

    3

--- backend/controller/Ctodo.js
+++ backend/controller/Ctodo.js
@@ -1,8 +1,10 @@
+const { where } = require('sequelize');
 const { todo } = require('../models/index')
 
 exports.getTodos = async (req, res) => {
     try {
         const todos = await todo.findAll();
+        res.json(todos)
     } catch (error) {
         console.error(error);
     }
@@ -15,6 +17,7 @@ exports.postTodos = async (req, res) => {
         const newTodo = await todo.create({
             title, done, createdAt, updatedAt
         });
+        res.json(newTodo);
     } catch (error) {
         console.error(error);
     }
@@ -22,7 +25,50 @@ exports.postTodos = async (req, res) => {
 
 exports.getTodo = async (req, res) => {
     try {
-        const todo = await todo.findOne()
+        console.log(req.params);
+        const { id } = req.params;
+        const todo1 = await todo.findOne({
+            where: { id }
+        });
+        res.json(todo1)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Todo not found')
+    }
+}
+
+exports.patchTodos = async (req, res) => {
+    try {
+        console.log(req.params);
+        console.log(req.body);
+
+        const { id } = req.params;
+        const { title, done } = req.body;
+
+        const updateTodo = await todo.update(
+            {done},
+            {where: {id}}
+        );
+        res.json(updateTodo)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+exports.deleteTodo = async (req, res) => {
+    try {
+        console.log(req.params);
+
+        const { id } = req.params;
+        const isDeleted = await todo.destroy({
+            where: { id }
+        })
+        console.log("isDeleted", isDeleted);
+        if (isDeleted) {
+            return res.send(true);
+        } else {
+            return res.send(false)
+        }
     } catch (error) {
         console.error(error);
     }
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -8,4 +8,8 @@ router.post('/todos', controller.postTodos);
 
 router.get('/todos/:id', controller.getTodo);
 
+router.patch('/todos/:id', controller.patchTodos);
+
+router.delete('/todos/:id', controller.deleteTodo);
+
 module.exports = router;
