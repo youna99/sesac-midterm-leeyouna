11e071da589a0b085a5f1250eccae71421cec56a
youna99 <dbsk0108@gmail.com>
2024-07-31 16:19:02 +0900

    최종

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -5,8 +5,8 @@ async function getTodos() {
             url: "https://jsonplaceholder.typicode.com/todos"
         })
         console.log(res.data);
-        let list = res.data.slice(0, 10);
-        const newLi = document.createElement('li');
+        // let list = res.data.slice(0, 10);
+        let arr = res.data;
     } catch (error) {
         console.error(error);
     }
@@ -19,46 +19,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const btn = document.getElementById('btn');
 let addValue = document.getElementById('addValue');
 let result = document.getElementById('result');
-
-// function addTodo () {
-//     if(window.event.keyCode === 13){
-//         let list = document.createElement("li");
-//         let del = document.createElement('button');
-//         list.innerHTML = addValue.value;
-//         result.appendChild(list); //추가된 할일에 할일 리스트 추가하기
-//         list.appendChild(del);    //할일 리스트 추가시 삭제버튼도 추가    
-//         del.innerText = "x";      //삭제버튼에 들어갈 'x'자 문자
-//         addValue.value = null
-//         // del.addEventListener("click", deleteList); //삭제버튼 클릭시 리스트지우기 이벤트 실행
-//     }
-// }
-
-// function keyCodeCheck () { // 엔터키로 추가
-// 	if(window.event.keyCode === 13 && addValue.value !== ''){
-//         addTodo();
-//     }
-// }
-
-// btn.addEventListener('click', () => { // + 버튼으로 추가
-//     if(addValue.value !== ''){ // 빈 값 입력 방지
-//         addTodo();
-//     }
-// })
-
-// function addTodo() {
-//     let list = document.createElement("li");
-//     let del = document.createElement('button');
-//     list.innerHTML = addValue.value;
-//     result.appendChild(list); 
-//     list.appendChild(del);      
-//     del.innerText = "x";      
-//     addValue.value = null
-//     del.addEventListener("click", deleteList);
-//     list.addEventListener('click', function() {
-//         list.style.textDecoration = "line-through";
-//         list.style.color = "gray";
-//     })
-// }
 
 const addBtn = document.querySelector('#addBtn');
 
