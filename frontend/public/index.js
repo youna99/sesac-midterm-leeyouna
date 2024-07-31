f7c3b586ced62b8f4b3819d2ddd34a29c618bbd9
youna99 <dbsk0108@gmail.com>
2024-07-31 15:50:33 +0900

    4

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,14 +1,16 @@
-// get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-//     console.log(res);
-// })
+function getTodos() {
+    axios({
+        method: get,
+        url: "https://jsonplaceholder.typicode.com/todos"
+    })
+    .then((res) => {
+    console.log(res);
+})
+}
 
-// function getTodos() {
-    
-// }
-
-// window.addEventListener("DOMContentLoaded", (event) => {
-//     getTodos()
-//   });
+window.addEventListener("DOMContentLoaded", (event) => {
+    getTodos()
+  });
 
 const btn = document.getElementById('btn');
 let addValue = document.getElementById('addValue');
@@ -27,19 +29,69 @@ let result = document.getElementById('result');
 //     }
 // }
 
-function addTodo() {
-    let list = document.createElement("li");
+// function keyCodeCheck () { // 엔터키로 추가
+// 	if(window.event.keyCode === 13 && addValue.value !== ''){
+//         addTodo();
+//     }
+// }
+
+// btn.addEventListener('click', () => { // + 버튼으로 추가
+//     if(addValue.value !== ''){ // 빈 값 입력 방지
+//         addTodo();
+//     }
+// })
+
+// function addTodo() {
+//     let list = document.createElement("li");
+//     let del = document.createElement('button');
+//     list.innerHTML = addValue.value;
+//     result.appendChild(list); 
+//     list.appendChild(del);      
+//     del.innerText = "x";      
+//     addValue.value = null
+//     del.addEventListener("click", deleteList);
+//     list.addEventListener('click', function() {
+//         list.style.textDecoration = "line-through";
+//         list.style.color = "gray";
+//     })
+// }
+
+const addBtn = document.querySelector('#addBtn'); // 추가 버튼 선택
+
+function keyCodeCheck (event) { // 엔터키로 추가
+	if(document.event.keyCode === 13 && todoInput.value !== ''){
+        addTodo();
+    }
+}
+
+addBtn.addEventListener('click', () => {
+    if(todoInput.value !== ''){ // 빈 값 입력 방지
+        addTodo();
+    }
+})
+
+function addTodo () { // 할 일 추가 기능
+	const todoList = document.querySelector('#todoList');
+	const newLi = document.createElement('li'); // li 생성
+    const newBtn = document.createElement('button'); // button 생성
+	const newSpan = document.createElement('span'); // span 생성
+	const todoInput = document.querySelector('#todoInput');
     let del = document.createElement('button');
-    list.innerHTML = addValue.value;
-    result.appendChild(list); 
-    list.appendChild(del);      
-    del.innerText = "x";      
-    addValue.value = null
+    del.innerText = "x";
     del.addEventListener("click", deleteList);
-    list.addEventListener('click', function() {
-        list.style.textDecoration = "line-through";
-        list.style.color = "gray";
-    })
+      
+    newLi.appendChild(newBtn); // li안에 button 담기
+	newLi.appendChild(newSpan); // li안에 span 담기
+      
+	newSpan.textContent = todoInput.value; // span 안에 value값 담기
+      
+	todoList.appendChild(newLi);
+      
+	todoInput.value = ''; // value 값에 빈 문자열 담기
+  
+	newBtn.addEventListener('click', () => { // 체크박스 클릭시 완료 표시
+		newLi.classList.toggle('complete');
+    });
 }
 
 function deleteList(e){
